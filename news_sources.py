ba595dc7f9771733656963b3c6ae939e06456435
khanalayush12 <khanalayush030@gmail.com>
2026-06-11 01:26:05 +0545

    Update news_sources.py

--- news_sources.py
+++ news_sources.py
@@ -1,4 +1,7 @@
-NEWS_FEEDS = [
-    "https://www.manutd.com/en/rss/news",
+# Manchester United News RSS Sources
+
+SOURCES = [
     "https://feeds.bbci.co.uk/sport/football/rss.xml",
+    "https://www.skysports.com/rss/12040",
+    "https://www.manutd.com/en/news/rss",
 ]
